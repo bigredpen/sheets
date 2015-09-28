2d4d7818e8ebc327014a0180b36e001807978edd
Big Red Pen, Inc. <github@bigredpen.com>
2015-09-28 10:31:01 -0700

    Update to .xls to .xlsx path correction

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -23,7 +23,7 @@ class SheetsController < ApplicationController
       if sheet.original_filename[/xls$/]
         if libreoffice_available?
           system "libreoffice --headless --convert-to xlsx #{sheet.tempfile.path} --outdir tmp"
-          path = sheet.tempfile.path[1..-4] + ".xlsx"
+          path = sheet.tempfile.path[1..-5] + ".xlsx"
         else
           flash[:error] = "Can't import MS Excel files without headless LibreOffice."
         end
