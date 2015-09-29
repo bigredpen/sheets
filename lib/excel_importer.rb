dc9465cb6ec577a1be4cd0b11096046a7cdf7689
Big Red Pen, Inc <info@bigredpen.com>
2015-09-29 14:20:30 -0700

    Remove module wrapper on ExcelImporter

--- lib/excel_importer.rb
+++ lib/excel_importer.rb
@@ -1,39 +1,35 @@
-module ExcelImporter
+class ExcelImporter
+  attr_reader   :path
+  attr_accessor :sheets
+
+  def initialize(path)
+    @path = path
+    @sheets = []
+  end
+
+  def run
+    x = RubyXL::Parser.parse(path) if path
   
-  class ExcelImporter
-    attr_reader   :path
-    attr_accessor :sheets
+    x.sheets.each_with_index do |xsheet,xno|
+      sheet = Sheet.new(name: xsheet.name, no_logging: true)
+      sheets << sheet
 
-    def initialize(path)
-      @path = path
-      @sheets = []
-    end
+      xrows = x[xno].sheet_data.rows
 
-    def run
-      x = RubyXL::Parser.parse(path) if path
-    
-      x.sheets.each_with_index do |xsheet,xno|
-        sheet = Sheet.new(name: xsheet.name, no_logging: true)
-        sheets << sheet
+      sheet.row_count     = xrows.size
+      sheet.column_count  = xrows.map(&:cells).map(&:size).sort.last
+      sheet.save
 
-        xrows = x[xno].sheet_data.rows
-
-        sheet.row_count     = xrows.size
-        sheet.column_count  = xrows.map(&:cells).map(&:size).sort.last
-        sheet.save
-
-        xrows.map(&:cells).each_with_index do |xrow,row|
-          xrow.each_with_index do |cell,col|
-            val = cell.value rescue nil
-            unless val.blank?
-              cell = sheet[row][col]
-              cell.content = val
-              cell.save
-            end
+      xrows.map(&:cells).each_with_index do |xrow,row|
+        xrow.each_with_index do |cell,col|
+          val = cell.value rescue nil
+          unless val.blank?
+            cell = sheet[row][col]
+            cell.content = val
+            cell.save
           end
         end
       end
     end
   end
-  
 end
