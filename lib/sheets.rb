1035ebfa61052ada8aac6a906287ff2f6667dd02
Big Red Pen, Inc <info@bigredpen.com>
2015-09-29 13:49:33 -0700

    Autoload engine lib files

--- lib/sheets.rb
+++ lib/sheets.rb
@@ -1,8 +1,5 @@
 require "sheets/engine"
 require "sheets/content_type_validator"
 
-require 'pusher'
-require 'RubyXL'
-
 module Sheets
 end
--- lib/sheets/engine.rb
+++ lib/sheets/engine.rb
@@ -1,4 +1,7 @@
 module Sheets
   class Engine < ::Rails::Engine
+    require 'pusher'
+    require 'RubyXL'
+    config.autoload_paths << File.expand_path("../../lib", __FILE__)
   end
 end
