b40904a4246fa0e26ef49a75333684780b4ddaf1
Big Red Pen, Inc <info@bigredpen.com>
2015-09-29 14:02:01 -0700

    Test config.autoload_paths

--- lib/sheets/engine.rb
+++ lib/sheets/engine.rb
@@ -2,6 +2,6 @@ module Sheets
   class Engine < ::Rails::Engine
     require 'pusher'
     require 'RubyXL'
-    config.autoload_paths << File.expand_path("../../lib", __FILE__)
+    config.autoload_paths << File.expand_path("../lib/some/path", __FILE__)
   end
 end
