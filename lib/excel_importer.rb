35a81ebd0b1ec4b8c49faa4fc42acc62cf8c9015
Big Red Pen, Inc <info@bigredpen.com>
2015-09-29 13:59:18 -0700

    Add module for ExcelImporter

--- lib/excel_importer.rb
+++ lib/excel_importer.rb
@@ -1,32 +1,34 @@
-class ExcelImporter
-  attr_reader   :path
-  attr_accessor :sheets
+module ExcelImporter
+  class ExcelImporter
+    attr_reader   :path
+    attr_accessor :sheets
 
-  def initialize(path)
-    @path = path
-    @sheets = []
-  end
+    def initialize(path)
+      @path = path
+      @sheets = []
+    end
 
-  def run
-    x = RubyXL::Parser.parse(path) if path
+    def run
+      x = RubyXL::Parser.parse(path) if path
     
-    x.sheets.each_with_index do |xsheet,xno|
-      sheet = Sheet.new(name: xsheet.name, no_logging: true)
-      sheets << sheet
+      x.sheets.each_with_index do |xsheet,xno|
+        sheet = Sheet.new(name: xsheet.name, no_logging: true)
+        sheets << sheet
 
-      xrows = x[xno].sheet_data.rows
+        xrows = x[xno].sheet_data.rows
 
-      sheet.row_count     = xrows.size
-      sheet.column_count  = xrows.map(&:cells).map(&:size).sort.last
-      sheet.save
+        sheet.row_count     = xrows.size
+        sheet.column_count  = xrows.map(&:cells).map(&:size).sort.last
+        sheet.save
 
-      xrows.map(&:cells).each_with_index do |xrow,row|
-        xrow.each_with_index do |cell,col|
-          val = cell.value rescue nil
-          unless val.blank?
-            cell = sheet[row][col]
-            cell.content = val
-            cell.save
+        xrows.map(&:cells).each_with_index do |xrow,row|
+          xrow.each_with_index do |cell,col|
+            val = cell.value rescue nil
+            unless val.blank?
+              cell = sheet[row][col]
+              cell.content = val
+              cell.save
+            end
           end
         end
       end
